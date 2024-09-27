9f9d0442ab2eae7f1e911eefd464128cf9fe6278
Sandro-it <kozachenkooleksandr@gmail.com>
2024-09-27 17:25:08 +0300

    key-back

--- config/server.ts
+++ config/server.ts
@@ -1,16 +1,15 @@
-// export default ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-// });
-
-export default {
-  host: '0.0.0.0',
-  port: 1337,
+export default ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: ['h+My0MpFJ2G/IXecFLo/mg==', 'x6ik+IZbqYFLjkzmCRSauw==', 'dIh9OKuB2KhEYK6UJJCA2A==', 'zzURGkVmT6UaBfyeM6To3A=='],
+    keys: env.array("APP_KEYS"),
   },
-};
+});
 
+// export default {
+//   host: '0.0.0.0',
+//   port: 1337,
+//   app: {
+//     keys: ['h+My0MpFJ2G/IXecFLo/mg==', 'x6ik+IZbqYFLjkzmCRSauw==', 'dIh9OKuB2KhEYK6UJJCA2A==', 'zzURGkVmT6UaBfyeM6To3A=='],
+//   },
+// };
