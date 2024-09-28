d103650abd6eb81506339009d34d7d86a4c2da8b
Sandro-it <kozachenkooleksandr@gmail.com>
2024-09-28 12:03:48 +0300

    serv

--- config/server.ts
+++ config/server.ts
@@ -1,15 +1,11 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  url: env(
+    "PUBLIC_URL",
+    "https://svitliidei-strapi-backend-production.up.railway.app"
+  ), // Додано публічний URL
   app: {
     keys: env.array("APP_KEYS"),
   },
 });
-
-// export default {
-//   host: '0.0.0.0',
-//   port: 1337,
-//   app: {
-//     keys: ['h+My0MpFJ2G/IXecFLo/mg==', 'x6ik+IZbqYFLjkzmCRSauw==', 'dIh9OKuB2KhEYK6UJJCA2A==', 'zzURGkVmT6UaBfyeM6To3A=='],
-//   },
-// };
