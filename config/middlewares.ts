7c62dce9c922d79087f6b058483a6d93d8425de8
Sandro-it <kozachenkooleksandr@gmail.com>
2024-09-28 11:12:32 +0300

    cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,22 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [
+        "http://localhost:3000", // ваш локальний домен React-аплікації
+        "https://your-production-domain.vercel.app", // замініть на ваш реальний домен Vercel
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
