05cb45dfa4601b98233d2aa19d2fa6eeb9cb3ab8
Sandro-it <kozachenkooleksandr@gmail.com>
2026-07-18 19:48:53 +0300

    fix: remove duplicate export default in config/middlewares.ts
    A previous edit through the GitHub web editor accidentally duplicated
    the whole file content, leaving two "export default" statements and
    breaking the Render deploy with TS2528. Collapsed back to a single
    export with the full CORS origin list (including localhost:5173 and
    svitli.com.ua) and the Cloudinary CSP directives.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,49 +1,4 @@
 export default ({ env }) => [
-    "strapi::logger",
-    "strapi::errors",
-  {
-        name: "strapi::security",
-        config: {
-                contentSecurityPolicy: {
-                          useDefaults: true,
-                          directives: {
-                                      "img-src": ["'self'", "data:", "blob:", "*.cloudinary.com"],
-                                      "media-src": ["'self'", "data:", "blob:", "*.cloudinary.com"],
-                                      upgradeInsecureRequests: null,
-                          },
-                },
-        },
-  },
-  {
-        name: "strapi::cors",
-        config: {
-                origin: [
-                          "http://localhost:3000",
-                          "http://localhost:5173",
-                          "https://svitli-idei.vercel.app",
-                          "https://svitli.com.ua",
-                          "https://www.svitli.com.ua",
-                          "https://light-concepts-frontend.vercel.app",
-                        ],
-                headers: [
-                          "Content-Type",
-                          "Authorization",
-                          "Origin",
-                          "Accept",
-                          "X-Requested-With",
-                        ],
-                methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-                credentials: true,
-        },
-  },
-    "strapi::poweredBy",
-    "strapi::query",
-    "strapi::body",
-    "strapi::session",
-    "strapi::favicon",
-    "strapi::public",
-  ];
-export default ({ env }) => [
   "strapi::logger",
   "strapi::errors",
   {
@@ -63,8 +18,12 @@ export default ({ env }) => [
     name: "strapi::cors",
     config: {
       origin: [
-        "http://localhost:3000", // ваш локальний домен React-аплікації
-        "https://svitli-idei.vercel.app", // ваш продакшн-домен
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://svitli-idei.vercel.app",
+        "https://svitli.com.ua",
+        "https://www.svitli.com.ua",
+        "https://light-concepts-frontend.vercel.app",
       ],
       headers: [
         "Content-Type",
